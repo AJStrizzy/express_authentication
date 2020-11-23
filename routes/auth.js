8e9054314d89c11fb7b436422f4e9b38adeaab98
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-11-23 15:59:50 -0500

    added login and failed login messages

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+// const passport = require('passport');
 const router = express.Router();
-
+const db = require ('../models')
+const passport = require('../config/ppConfig')
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
@@ -9,4 +11,36 @@ router.get('/login', (req, res) => {
   res.render('auth/login');
 });
 
+router.post ('/signup', (req, res) => {
+  console.log(req.body)
+  db.user.findOrCreate({
+    where: { email : req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(([user, created]) => {
+    if (created) {
+      console.log(`${user.name} was created`)
+      const successObject = {
+        successRedirect: '/', 
+        successFlash: 'Account created and logging in...'
+      }
+      passport.authenticate('local', successObject) (req, res)  
+    } else {
+      // email already exists
+         req.flash('error', 'Email already exists...')
+         res.redirect('/auth/signup');
+    
+    }
+  })
+  .catch(err => {
+    console.log('Error', err)
+    req.flash('error', 'Either email or password is incorrect. Please try again')
+    res.redirect('/auth/signup')
+  })
+})
+
 module.exports = router;
+
