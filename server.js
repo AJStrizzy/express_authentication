d47e0847ee45ee59566e148825c83bf3aa96dbb3
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-11-23 14:57:41 -0500

    [server] added flash to home route

--- server.js
+++ server.js
@@ -26,11 +26,30 @@ const sessionObject = {
 
 app.use (session(sessionObject));
 
+// Initialize passport and run through middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// using throughout app to send temp messages to user
+app.use(flash());
+
+// Messages that will be accessible to every view
+app.use((req, res, next) => {
+  //berfore every route we will attach a user to res.local
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+})
+
+
 app.get('/', (req, res) => {
-  res.render('index');
+  console.log(res.locals.alerts);
+  res.render('index', { alerts: res.locals.alerts });
 });
 
-app.get('/profile', (req, res) => {
+app.get('/profile', isLoggedIn, (req, res) => {
+
   res.render('profile');
 });
 
